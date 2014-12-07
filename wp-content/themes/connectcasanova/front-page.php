67657deb5c25714ff39f61393db48cd807fb472b
abe <abe@thehonestape.com>
2014-12-07 01:13:30 -0500

    name change for theme

---
+++ wp-content/themes/connectcasanova/front-page.php
@@ -0,0 +1,10 @@
+<div class="row">
+  <div class="col-lg-8 col-lg-offset-3">
+    <div class="branding"></div>
+    <div class="row section">
+      <h3>What?</h3>
+      <p>Dear Casanova Resident,</p>
+      <p>Many of you attended the meeting at Parish Hall concering the proposed Verizon tower.</p>
+    </div>
+  </div>
+</div>
--- wp-content/themes/connectcasanova/lib/scripts.php
+++ wp-content/themes/connectcasanova/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b8624aef94ef66f5cd808b69ee997816');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '9d8583b80bfadc53241301b03f897c97');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
