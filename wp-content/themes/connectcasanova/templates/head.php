8f37fec5e1e986e4c6b85aa09d560a7820263651
abe <abe@thehonestape.com>
2015-02-01 03:16:12 -0500

    google verification

--- wp-content/themes/connectcasanova/templates/head.php
+++ wp-content/themes/connectcasanova/templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="google-site-verification" content="CQfo-Kk4_g-CW5hLuXHy_PXul_5KBr_1b8o0FL0VBeE" />
 
   <?php wp_head(); ?>
 
