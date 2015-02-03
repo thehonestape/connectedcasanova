60c9b15de1575a7f557f02453f969dfd327a81bb
abe <abe@thehonestape.com>
2015-02-03 01:32:12 -0500

    favicons
    testing favicons

--- wp-content/themes/connectcasanova/templates/head.php
+++ wp-content/themes/connectcasanova/templates/head.php
@@ -6,6 +6,11 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="google-site-verification" content="CQfo-Kk4_g-CW5hLuXHy_PXul_5KBr_1b8o0FL0VBeE" />
+  <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png">
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon">
+
+
 
   <?php wp_head(); ?>
 
