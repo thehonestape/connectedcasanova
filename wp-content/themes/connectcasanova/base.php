9faf870ba5e0483e9637b7cf4de2084c410f0b0a
abe <abe@thehonestape.com>
2015-02-25 10:27:35 -0500

    front page updates

--- wp-content/themes/connectcasanova/base.php
+++ wp-content/themes/connectcasanova/base.php
@@ -49,8 +49,9 @@
             <p class="lead">This website is the voice of <em>The Neighborhood Committee for Stronger, More Reliable Data and Voice Service</em>.</p> 
             <p>The committee consists of Casanova residents responding to inadequate, unreliable cell and internet signal. We are not affiliated with any carrier. Verizon is the only carrier currently offering a solution, and we support the proposed 150 ft Verizon Tower, to be located in Casanova. We currently speak for over 90% of residents living within one mile of the center of the village.
              </p>
-             <p> 
-            <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact Spokesman John D. Miller</a>
+             <p>
+              <span>Spokesman: John D. Miller</span><br>
+            <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact</a>
             </p>
           </div>
           </div>
@@ -62,7 +63,8 @@
             <p>The committee consists of Casanova residents responding to inadequate, unreliable cell and internet signal. We are not affiliated with any carrier. Verizon is the only carrier currently offering a solution, and we support the proposed 150 ft Verizon Tower, to be located in Casanova. We currently speak for over 90% of residents living within one mile of the center of the village.<br>
             </p>
             <p>
-            <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact Spokesman John D. Miller</a>
+              <span class="small">Spokesman: John D. Miller</span><br>
+            <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact</a>
             </p>
           </div>
         </div>
