95343f41c6c9501678c8e8f06eee7118d2a80542
abe <abe@thehonestape.com>
2015-03-02 14:05:58 -0500

    take action button

--- wp-content/themes/connectcasanova/base.php
+++ wp-content/themes/connectcasanova/base.php
@@ -48,8 +48,7 @@
             <div class="intro">
             <h4 class="title">About</h4>
             <p class="lead">This website is the voice of <em>The Neighborhood Committee for Stronger, More Reliable Data and Voice Service</em>.</p> 
-            <p>The committee consists of Casanova residents responding to inadequate, unreliable cell and internet signal. We are not affiliated with any carrier. Verizon is the only carrier currently offering a solution, and we support the proposed 150 ft Verizon Tower, to be located in Casanova. We currently speak for over 90% of residents living within one mile of the center of the village.
-             </p>
+
              <p>
             <a href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova"><span>Spokesman: John D. Miller, jdmillerjack@gmail.com</span></a><br>
             <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact</a>
@@ -61,8 +60,7 @@
           <div class="intro">
             <h4 class="title">About</h4>
             <p class="lead">This website is the voice of <em>The Neighborhood Committee for Stronger, More Reliable Data and Voice Service</em>.</p> 
-            <p>The committee consists of Casanova residents responding to inadequate, unreliable cell and internet signal. We are not affiliated with any carrier. Verizon is the only carrier currently offering a solution, and we support the proposed 150 ft Verizon Tower, to be located in Casanova. We currently speak for over 90% of residents living within one mile of the center of the village.<br>
-            </p>
+          
             <p>
             <a href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova"><span>Spokesman: John D. Miller, jdmillerjack@gmail.com</span></a><br>
             <a class="btn btn-lg btn-default" href="mailto:jdmillerjack@gmail.com?Subject=Connect Casanova" target="_blank"> Contact</a>
--- wp-content/themes/connectcasanova/lib/scripts.php
+++ wp-content/themes/connectcasanova/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6e00e860f7b30cd815dcc2c9633632d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '652685beb44d972f93ad08670fc80065');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
