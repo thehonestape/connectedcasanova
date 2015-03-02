e58feb3b962126ca7ae0d2fa46aab50306171c1d
abe <abe@thehonestape.com>
2015-03-02 13:45:46 -0500

    larger nav font size

--- wp-content/themes/connectcasanova/lib/scripts.php
+++ wp-content/themes/connectcasanova/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd51332763e5d4742cd211a73783b05b1');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e6e00e860f7b30cd815dcc2c9633632d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
