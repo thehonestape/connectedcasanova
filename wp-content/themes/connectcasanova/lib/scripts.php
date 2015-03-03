365edbd897f6f995e6774b09381402654b3778f8
abe <abe@thehonestape.com>
2015-03-03 01:09:33 -0500

    center menu

--- wp-content/themes/connectcasanova/lib/scripts.php
+++ wp-content/themes/connectcasanova/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '263b9785df3186ba1e9279f592616eef');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5e6cabe500ff03290e8301379bc144e2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
