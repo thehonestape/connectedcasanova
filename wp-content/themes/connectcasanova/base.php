8af61e985f2ea152750942ab8ba4e580246f2caa
abe <abe@thehonestape.com>
2015-03-23 09:54:12 -0400

    fb script

--- wp-content/themes/connectcasanova/base.php
+++ wp-content/themes/connectcasanova/base.php
@@ -1,5 +1,13 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=594798277273886&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
